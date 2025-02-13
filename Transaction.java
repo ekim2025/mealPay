b23977470c39c74077862860eeda3dc3c006c63f
Evelyn Kim <ekim25@students.polytechnic.org>
2025-02-13 10:20:23 -0800

    toString method

--- Transaction.java
+++ Transaction.java
@@ -1,5 +1,24 @@
 public class Transaction{
     private String date;
     private double amount;
-    private int 
+    private int studentId;
+
+    public Transaction(String date, double amount, int studentId) {
+        date = date;
+        amount = amount;
+        studentId = studentId;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
 }
