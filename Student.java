19c41ac122b2628bd81e8b9e04905e69d75845f9
Evelyn Kim <ekim25@students.polytechnic.org>
2025-02-19 14:31:54 -0800

    student constructor

--- Student.java
+++ Student.java
@@ -1,11 +1,19 @@
 import java.util.*;
 public class Student{
-
+    //fields
     private String name;
     private int id;
     private static int idGen = 1;
     private ArrayList<Transaction> transactions;
     private double balance;
+
+    //constructor
+    public Student(String myName){
+        name = myName;
+        balance = 0;
+        id = idGen;
+        idGen++;
+    }
 
 
 
